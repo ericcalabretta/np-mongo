3b1a09978c3bc3c20a281ae175acb672bb3ebc92
Eric Calabretta <ericcalabretta@users.noreply.github.com>
2018-01-11 14:12:00 -0800

    Update plan.sh

--- plan.sh
+++ plan.sh
@@ -1,8 +1,8 @@
-pkg_origin=echohack
+pkg_origin=ericcalabretta
 pkg_name=np-mongodb
 pkg_version=3.2.9
 pkg_description="Mongodb for National-Parks app"
-pkg_maintainer="echohack"
+pkg_maintainer="Eric Calabretta"
 pkg_license=('AGPL-3.0')
 pkg_deps=(core/mongodb core/mongo-tools)
 pkg_svc_run="mongod --config $pkg_svc_config_path/mongod.conf"
